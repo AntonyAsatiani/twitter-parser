0a2b44da45fd932e1fef86490a1cbf022fec6c21
A-Asatiani <asatiani.websmart@gmail.com>
2022-07-30 14:49:35 +0400

    change to path parameter

--- middleware/requestValidator.js
+++ middleware/requestValidator.js
@@ -1,7 +1,7 @@
 const requestValidator = (req, res, next) => {
 const regex = /^[a-zA-Z0-9]+$/
 
-if (!regex.test(req.query.username))
+if (!regex.test(req.params.username))
     return res.status(400).send({
       status: '400',
       error: 'Bad Request',
--- parseTwitterUser.js
+++ parseTwitterUser.js
@@ -4,9 +4,9 @@ const app = express();
 const parseTwitterService = require('./services/twitter.service');
 const requestValidator = require('./middleware/requestValidator');
 
-app.get('/twitter', requestValidator, async (req, res) => {
+app.get('/twitter/:username', requestValidator, async (req, res) => {
   try {
-    const responseFromTwitter = await parseTwitterService(req.query.username);
+    const responseFromTwitter = await parseTwitterService(req.params.username);
     if (responseFromTwitter.errors) {
       return res.status(404).send({
         error: 'Resource Not Found',
